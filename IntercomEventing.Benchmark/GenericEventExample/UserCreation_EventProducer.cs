eb158b50994d41ca5d9fb9aab058ebcbb9e7a07e
Jon Owens <15575425+DragoQCC@users.noreply.github.com>
2025-06-15 16:34:46 -0400

    updated package references
    updated package references

--- IntercomEventing.Benchmark/GenericEventExample/UserCreation_EventProducer.cs
+++ IntercomEventing.Benchmark/GenericEventExample/UserCreation_EventProducer.cs
@@ -4,8 +4,9 @@ public class UserCreation_EventProducer
 {
     public UserCreatedEvent UserCreatedEvent { get; init; } = new();
     public UserThresholdReachedEvent UserThresholdReachedEvent { get; init; } = new();
+    
 
-    public async Task CreateUserAsync(User user) => await UserCreatedEvent.RaiseEvent(new UserCreatedEventCall(user),this);
+    public async Task CreateUserAsync(User user) => UserCreatedEvent.NotifyUserCreated(user);
 
     public async Task NotifyUserThresholdReachedAsync(User user) => await UserThresholdReachedEvent.RaiseEvent(new UserThresholdReachedEventCall(user));
 }
